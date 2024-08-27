6d0594111ff49d75986cf2f3448a8145b5bd4b7a
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-27 11:58:58 +0530

    done 1.5

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from 'react'
+import Mark1 from './Mark1'
+import Mark2 from './Mark2'
 
-const App = () => {
-  const [text, setText] = useState(""); // State to store markdown text
-
-  // Handler for updating the text state when textarea value changes
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  return (
-    <div className="app">
-      <div className="markdown-container">
-        {/* Textarea for writing markdown */}
-        <textarea
-          className="textarea"
-          onChange={handleTextChange}
-          placeholder="Enter your markdown here..."
-        ></textarea>
-        
-        {/* Preview section */}
-        <div className="preview">
-          <h1>Markdown Preview</h1>
-          <p>{text}</p>
+let App= ()=>{
+    return (
+        <div className='app'>
+            <Mark1/>
+            {/* <Mark2 /> */}
         </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+    )
+}
+export default App
